198612be27dad278a9a049e3a3cc2698a5dab00d
Tu Anh <duongtuanh130197@gmail.com>
2026-04-06 19:16:28 +0700

    fix: dbConnect import leading to build failure on Vercel

--- src/app/api/hack/route.ts
+++ src/app/api/hack/route.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
-import { connectDB } from "@/lib/mongodb";
+import dbConnect from "@/lib/mongodb";
 import User from "@/models/User";
 
 export async function GET() {
-  await connectDB();
+  await dbConnect();
   const result = await User.updateOne(
     { email: "huanfpt03@gmail.com" },
     { $set: { balance: 10000000, name: "DUONG TUANH" } }
